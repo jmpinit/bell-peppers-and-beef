9ea915adfd8f986c16c004dce2b3f1f31df6df8d
Owen Trueblood <trueowen@verizon.net>
2015-03-07 21:57:44 -0500

    More readable.

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,18 @@ var fs = require('fs');
 var PostBuilder = require('./gulp/post-builder');
 var Tandem = require('./gulp/tandem');
 
-// TODO autogenerate watch tasks
-
 gulp.task('posts', function() {
     var postTemplate = fs.readFileSync('templates/post.mustache', 'utf8');
     var posts = gulp.src('posts/*.md').pipe(markdown());
     var metas = gulp.src('posts/*.json');
 
-    return Tandem({'body': posts, 'meta': metas}).pipe(PostBuilder(postTemplate)).pipe(gulp.dest('www/'));
+    return Tandem({'body': posts, 'meta': metas})
+        .pipe(PostBuilder(postTemplate))
+        .pipe(gulp.dest('www/'));
 });
 
-gulp.task('watch-posts', ['posts'], function() {
+gulp.task('watch', ['posts'], function() {
     gulp.watch('posts/*', ['posts']);
-});
-
-gulp.task('watch-templates', function() {
     gulp.watch('templates/*', ['posts']);
 });
 
